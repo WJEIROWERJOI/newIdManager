da5cf4682f6468d3a187c398686b5e716f125f46
jiwon <kgwball@hotmail.com>
2025-04-27 02:06:36 +0900

    hello world!

---
+++ Data/Posts/CommentDto.cs
@@ -0,0 +1,12 @@
+namespace newIdManager.Data.Posts
+{
+    public class CommentDto
+    {
+        public int Id { get; set; }
+        public int PostId { get; set; }
+        public string Content { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        // public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    }
+}
